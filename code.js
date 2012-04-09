8dd225e4dfaf5a5121f4b2c0aa09f0b9c8d0b85c
Mikhail Saburenkov <m.saburenkov@hh.ru>
2012-04-09 23:57:38 +0400

    first commit

---
+++ code.js
@@ -0,0 +1,21 @@
+var div = document.body.appendChild(document.createElement('div'));
+div.innerHTML = 'Saving...';
+div.style.position = 'fixed';
+div.style.height = '100%';
+div.style.width = '100%';
+div.style.top = 0;
+div.style.left = 0;
+div.style.zIndex = 1000;
+div.style.fontSize = '40px';
+div.style.textAlign = 'center';
+div.style.fontWeight = 'bold';
+div.style.color = '#fff';
+div.style.paddingTop = '10%';
+div.style.fontFamily = 'sans-serif';
+div.style.backgroundColor = 'rgba(0,0,0,.5)';
+setTimeout(function(){
+	div.innerHTML = 'OK!';
+	setTimeout(function(){
+		div.parentNode.removeChild(div);
+	}, 1000);
+}, 1000);
